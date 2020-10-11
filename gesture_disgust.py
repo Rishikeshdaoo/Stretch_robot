d3ae58b35d6758d47cfa9d7646e98b36e00a053f
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-10-11 15:53:54 -0400

    trying rotational motion

--- gesture_disgust.py
+++ gesture_disgust.py
@@ -8,18 +8,18 @@ from pydub.playback import play
 
 robot=stretch_body.robot.Robot()
 
-print("Playing sound...")
-sound = AudioSegment.from_file("./audios/Disgust.wav")
-sound.apply_gain(-6)
-play(sound)
+# print("Playing sound...")
+# sound = AudioSegment.from_file("./audios/Disgust.wav")
+# sound.apply_gain(-6)
+# play(sound)
 
 print("Doing gestures now...")
 
 robot.startup()
 
-robot.base.translate_by(x_m=0.5)
-robot.push_command()
-time.sleep(4.0) #wait
+# robot.base.translate_by(x_m=0.5)
+# robot.push_command()
+# time.sleep(4.0) #wait
 
 robot.base.set_rotational_velocity(v_r=0.1) #switch to velocity controller
 robot.push_command()
