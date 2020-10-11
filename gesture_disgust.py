ffcfcf6fac627f71e06095f4605ea229f9fb0d7f
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-10-11 15:49:47 -0400

    changes to disgust

--- gesture_disgust.py
+++ gesture_disgust.py
@@ -12,24 +12,20 @@ print("Playing sound...")
 sound = AudioSegment.from_file("./audios/Disgust.wav")
 sound.apply_gain(-6)
 play(sound)
-playsound(wav_file)
 
 print("Doing gestures now...")
-disgust();
+
+robot.startup()
+
+robot.base.translate_by(x_m=0.5)
+robot.push_command()
+time.sleep(4.0) #wait
+
+robot.base.set_rotational_velocity(v_r=0.1) #switch to velocity controller
+robot.push_command()
+time.sleep(4.0) #wait
+
+robot.base.set_rotational_velocity(v_r=0.0) #stop motion
+robot.push_command()
 
 robot.stop()
-
-
-def disgust():
-    robot.startup()
-
-    robot.base.translate_by(x_m=0.5)
-    robot.push_command()
-    time.sleep(4.0) #wait
-
-    robot.base.set_rotational_velocity(v_r=0.1) #switch to velocity controller
-    robot.push_command()
-    time.sleep(4.0) #wait
-
-    robot.base.set_rotational_velocity(v_r=0.0) #stop motion
-    robot.push_command()
