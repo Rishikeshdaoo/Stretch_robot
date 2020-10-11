9f27282169ec9be19e17b88e5bdce408db593cd7
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-10-11 12:19:41 -0700

    changes to movement test script

--- test_script.py
+++ test_script.py
@@ -12,9 +12,9 @@ wav_file = input("audios/Joy.wav")
 playsound(wav_file)
 
 print("Moving base now...")
-robot.base.translate_by(-0.5, 1)
+#robot.base.translate_by(-0.5, 1)
 
-robot.push_command()
+#robot.push_command()
 
 time.sleep(10)
 
