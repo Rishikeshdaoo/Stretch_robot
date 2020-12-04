69d1972b9b8a4b1bbaf80d0e9b5b6a383bafd55f
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-12-04 12:09:02 -0800

    commit repo to main

---
+++ gesture_disgust.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import stretch_body.robot
+import time
+import pydub
+from pydub import AudioSegment
+from pydub.playback import play
+from multiprocessing import Process
+
+# robot=stretch_body.robot.Robot()
+
+def disgust_audio():
+    print("Playing sound...")
+    sound = AudioSegment.from_file("./audios/Disgust_1.wav")
+    sound.apply_gain(100)
+    play(sound)
+    return
+
+def disgust_gesture(robot):
+    arm_vel_slow_m = robot.arm.params['motion']['slow']['vel_m']
+    arm_accel_slow_m = robot.arm.params['motion']['slow']['accel_m']
+    arm_vel_default_m = robot.arm.params['motion']['default']['vel_m']
+    arm_accel_default_m = robot.arm.params['motion']['default']['accel_m']
+    arm_vel_fast_m = robot.arm.params['motion']['fast']['vel_m']
+    arm_accel_fast_m = robot.arm.params['motion']['fast']['accel_m']
+
+    print("Doing gestures now...")
+
+    # robot.startup()
+
+    robot.head.move_to('head_pan', 2.14)
+    robot.end_of_arm.move_to('wrist_yaw', 0)
+    robot.base.rotate_by(x_r=1)
+    robot.lift.move_to(x_m=1)
+    robot.arm.move_to(x_m=0.3, v_m=arm_vel_fast_m)
+    robot.push_command()
+    time.sleep(3.5)
+    robot.head.move_to('head_pan', -1)
+    robot.base.rotate_by(x_r=-1)
+    robot.lift.move_to(x_m=0.7)
+    robot.arm.move_to(x_m=0)
+    robot.push_command()
+    time.sleep(6)
+
+    # robot.stow()
+    # robot.stop()
+
+    return
+
+if __name__ == "__main__":
+    # Create two threads as follows
+    try:
+        robot=stretch_body.robot.Robot()
+        robot.startup()
+        robot.stow()
+
+        p1 = Process(target=disgust_audio)
+        p1.start()
+        p2 = Process(target=disgust_gesture(robot))
+        p2.start()
+
+        robot.stop()
+    except:
+       print("Error: unable to start thread")
---
+++ gesture_fear.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import stretch_body.robot
+import time
+import pydub
+from pydub import AudioSegment
+from pydub.playback import play
+from multiprocessing import Process
+
+
+# robot=stretch_body.robot.Robot()
+
+def fear_audio():
+    print("Playing sound...")
+    sound = AudioSegment.from_file("./audios/Fear_1.wav")
+    sound.apply_gain(100)
+    play(sound)
+    return
+
+def fear_gesture(robot):
+    base_vel_fast_m = robot.base.params['motion']['fast']['vel_m']
+    base_accel_fast_m = robot.base.params['motion']['fast']['accel_m']
+
+    print("Doing gestures now...")
+
+    # robot.startup()
+
+    robot.head.move_to('head_pan', 3.1)
+    robot.base.translate_by(x_m=-0.5, v_m=base_vel_fast_m, a_m=base_accel_fast_m)
+    robot.lift.move_to(x_m=0.8)
+    robot.arm.move_to(x_m=0.5)
+    robot.push_command()
+    time.sleep(3)
+
+    robot.base.translate_by(x_m=-0.2, v_m=base_vel_fast_m, a_m=base_accel_fast_m)
+    robot.lift.move_to(x_m=0)
+    robot.arm.move_to(x_m=0.2)
+    robot.push_command()
+
+    time.sleep(4)
+
+    # robot.stow()
+    # robot.stop()
+
+    return
+
+if __name__ == "__main__":
+    # Create two threads as follows
+    try:
+        robot=stretch_body.robot.Robot()
+        robot.startup()
+
+        p1 = Process(target=fear_audio)
+        p1.start()
+        p2 = Process(target=fear_gesture)
+        p2.start()
+
+        robot.stop()
+    except:
+       print("Error: unable to start thread")
